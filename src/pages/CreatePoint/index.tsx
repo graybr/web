842de58548a0955687d0af7eddab4801317295d2
Adelson Neto <64606200+PoloEmbrapii@users.noreply.github.com>
2020-06-04 22:18:23 -0300

    finalizando

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi'
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker} from 'react-leaflet';
+import api from '../../services/api';
 
 import './style.css';
 
 import logo from '../../assets/logo.svg';
 
 const CreatePoint = () => {
+  useEffect(() => {
+    api.get('items').then(response => {
+      console.log(response);
+    });
+  }, []);
+
   return (
     <div id="page-create-point">
       <header>
@@ -58,6 +66,14 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
+          <Map center={[-17.8003316, -50.9393447]} zoom={15}>
+            <TileLayer 
+              attribution='&amp;copy
+              <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </Map>
+
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
@@ -82,10 +98,35 @@ const CreatePoint = () => {
           
           <ul className="items-grid">
             <li>
-              
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li className="selected">
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+              <span>Óleo de Cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+              <span>Óleo de Cozinha</span>
             </li>
           </ul>
         </fieldset>
+
+        <button>
+          Cadastrar ponto de coleta
+        </button>
       </form>
     </div>
   );
